b1122fafcca47b1b1df1ad7655a3604291691f29
Deckinho <dericklucasrf@gmail.com>
2026-04-26 20:59:24 -0300

    Update funcoes.py

--- dados_e_funcoes/funcoes.py
+++ dados_e_funcoes/funcoes.py
@@ -2,3 +2,12 @@
 Arquivo voltado única e exclusivamente para a criação de funções usando o python base.
 Assim, o código fica mais fluido e limpo.
 '''
+
+def teste():
+  return(print("oii"))
+
+
+
+
+
+
